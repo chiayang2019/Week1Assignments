11f8d2b1a2f84fb51f55e81781488c51f6ea5ac3
chiayang <chiayang@100.76.100.115>
2019-09-25 17:07:11 -0400

    Initial commit of LMS System.

---
+++ LMSSystem/src/com/ss/lmscontrollers/LmsSystemMain.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.ss.lmscontrollers;
+
+/**
+ * @author chiayang
+ *
+ */
+public class LmsSystemMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
